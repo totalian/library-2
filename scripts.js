3666d0b8c498722a9ffba5fe875dc5d170b13741
Zeki Mirza <zeki.mirza@googlemail.com>
2021-10-02 00:06:29 +0100

    finished functionaluty but code is horrible

--- scripts.js
+++ scripts.js
@@ -3,7 +3,7 @@ const addBookBtn = document.querySelector('.add-book')
 const formContainer = document.querySelector('.form-container')
 const form = document.querySelector('form')
 
-let myLibrary = []
+let myLibrary = localStorage.myLibrary ? JSON.parse(localStorage.myLibrary) : []
 
 function Book(author,title,numPages,isRead){
     this.author = author
@@ -18,15 +18,33 @@ const renderLibrary = () => {
     while(library.firstChild){
         library.removeChild(library.lastChild)
     }
-    myLibrary.forEach(book => {
+    myLibrary.forEach((book,index) => {
         let renderedBook = document.createElement('div')
         let readMessage = book.isRead ? 'You have read this' : 'You have not read this'
+        let removeButton = document.createElement('button')
+        removeButton.textContent = "Remove"
+        let toggleReadButton = document.createElement('button')
+        toggleReadButton.textContent = "Change read"
         renderedBook.classList.toggle('card')
         if(book.isRead){renderedBook.classList.toggle('read')}
         renderedBook.textContent =
             `Title: ${book.title} \r\n Author: ${book.author} \r\n ${book.numPages} pages \r\n ${readMessage}`
+        renderedBook.appendChild(toggleReadButton)
+        renderedBook.appendChild(removeButton)
+        renderedBook.dataset.indexNumber = index
         library.appendChild(renderedBook)
+
+        removeButton.addEventListener('click', () => {
+            myLibrary.splice(index,1)
+            renderLibrary()
+        })
+
+        toggleReadButton.addEventListener('click', () => {
+            book.isRead = !book.isRead
+            renderLibrary()
+        })
     })
+    localStorage.setItem('myLibrary',JSON.stringify(myLibrary))
 }
 
 formContainer.addEventListener('click',(e) => {
@@ -46,13 +64,16 @@ form.addEventListener('submit', (e) => {
     let newBookTitle = document.querySelector('#title').value
     let newBookAuthor = document.querySelector('#author').value
     let newBookPagenum = document.querySelector('#pageNum').value
-    let newBookHasRead = document.querySelector('#hasRead').value
+    let newBookHasRead = document.querySelector('#hasRead').checked == true
     let newBook = new Book(newBookAuthor,newBookTitle,newBookPagenum,newBookHasRead)
+    console.log(newBook)
     addBookToLibrary(newBook)
     renderLibrary()
     form.reset()
     formContainer.style.display = "none"
 })
+
+renderLibrary()
 
 // // Test case
 // let lotr = new Book('Tolkein','Lord of the Rings',120,true)
