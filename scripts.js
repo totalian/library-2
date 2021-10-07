c36e25c09c1a7bdc2a3f3e4cc84372560184067f
Zeki Mirza <zeki.mirza@googlemail.com>
2021-10-07 20:17:36 +0100

    changed book to class

--- scripts.js
+++ scripts.js
@@ -5,11 +5,20 @@ const form = document.querySelector('form')
 
 let myLibrary = localStorage.myLibrary ? JSON.parse(localStorage.myLibrary) : []
 
-function Book(author,title,numPages,isRead){
-    this.author = author
-    this.title = title
-    this.numPages = numPages
-    this.isRead = isRead
+// function Book(author,title,numPages,isRead){
+//     this.author = author
+//     this.title = title
+//     this.numPages = numPages
+//     this.isRead = isRead
+// }
+
+class Book {
+    constructor(author,title,numPages,isRead){
+        this.author = author
+        this.title = title
+        this.numPages = numPages
+        this.isRead = isRead
+    }
 }
 
 const addBookToLibrary = book => myLibrary.push(book)
@@ -74,20 +83,3 @@ form.addEventListener('submit', (e) => {
 })
 
 renderLibrary()
-
-// // Test case
-// let lotr = new Book('Tolkein','Lord of the Rings',120,true)
-// let lotr2 = new Book('Salvator','Lord of the Things',1210,false)
-// let lotr3 = new Book('Rowling','Lord of the Kings',12110,true)
-// let lotr4 = new Book('Kingly','Lord of the Wings',130,false)
-// let lotr5 = new Book('Kingly','Lord of the Wings',130,false)
-// let lotr6 = new Book('Kingly','Lord of the Wings',130,false)
-// let lotr7 = new Book('Kingly','Lord of the Wings',130,false)
-
-// addBookToLibrary(lotr)
-// addBookToLibrary(lotr2)
-// addBookToLibrary(lotr3)
-// addBookToLibrary(lotr4)
-// addBookToLibrary(lotr5)
-// addBookToLibrary(lotr6)
-// addBookToLibrary(lotr7)
